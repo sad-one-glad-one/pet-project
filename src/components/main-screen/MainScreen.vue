2bb32a211f38a6297b81b587c6660e0f36e95869
@silnov_aleksey <silnov_aleksey@smartworld.team>
2022-06-22 19:14:53 +0700

    added vue-easy-lightbox

--- src/components/main-screen/MainScreen.vue
+++ src/components/main-screen/MainScreen.vue
@@ -2,7 +2,12 @@
   <article class="main-screen">
     <section class="main-screen__section">
       <div class="main-screen__img-wrap">
-        <img :src="selectedObject?.url" alt="main-img" draggable="false">
+        <img
+          :src="selectedObject?.url"
+          alt="main-img"
+          draggable="false"
+          @click="isZoomModalOpened = true"
+        >
       </div>
       <div class="main-screen__title">
         <p>{{ selectedObject?.title }}</p>
@@ -12,19 +17,39 @@
         <p>{{ selectedObject?.explanation }}</p>
       </div>
     </section>
+    <vue-easy-lightbox
+        :visible="isZoomModalOpened"
+        :imgs="isHdFormat ? selectedObject?.hdurl : selectedObject?.url"
+        @hide="isZoomModalOpened = false"
+    />
   </article>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 
+import VueEasyLightbox from 'vue-easy-lightbox'
+
 export default defineComponent({
   name: "MainScreen",
+  components: {
+    VueEasyLightbox
+  },
   props: {
     selectedObject: {
       type: Array
     }
   },
+  data () {
+    return {
+      isZoomModalOpened: false,
+    }
+  },
+  methods: {
+    isHdFormat (hd: string) : boolean {
+      return hd !== ''
+    }
+  }
 })
 </script>
 
